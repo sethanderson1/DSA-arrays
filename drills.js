547e33d3d7269428f809a2a2f29cdb0946722386
Seth Anderson <sdande02@gmail.com>
2020-07-21 19:41:02 -0700

    drills

--- drills.js
+++ drills.js
@@ -168,4 +168,133 @@ function mergeSorted(a, b) {
     return answer;
 }
 
-console.log('mergeSorted(arr1,arr2)', mergeSorted(arr1,arr2))
+console.log('mergeSorted(arr1,arr2)', mergeSorted(arr1, arr2))
+
+// remove a character
+
+function removeCharacter(string, letters) {
+    const letterArr = []
+    const stringArr = []
+
+    for (let i = 0; i < letters.length; i++) {
+
+        letterArr.push(letters.charAt(i))
+    }
+    for (let i = 0; i < string.length; i++) {
+
+        stringArr.push(string.charAt(i))
+    }
+    letterArr.forEach(letter => {
+        stringArr.forEach((let, index) => {
+            if (letter === let) {
+                stringArr.splice(index, 1)
+            }
+        })
+    })
+
+    const concatString = stringArr.reduce((acc, curr) => {
+        return acc + curr
+    })
+
+    return concatString
+}
+
+console.log('removeCharacter(\'Battle of the Vowels: Hawaii vs. Grozny\', \'aeiou\')', removeCharacter('Battle of the Vowels: Hawaii vs. Grozny', 'aeiou'))
+
+// products
+
+function productArr(arr) {
+    const product = arr.map((num, i) => {
+        const remainingNums = arr.slice(0, i).concat(arr.slice((i + 1), (arr.length)))
+        return remainingNums.reduce((acc, cur) => acc * cur)
+    })
+    return product
+}
+
+console.log('productArr([108, 36, 12, 27])', productArr([108, 36, 12, 27]))
+
+
+// 2d Array 
+
+function array2D(arr) {
+    const origArr = JSON.parse(JSON.stringify(arr))
+    let newArr = arr
+
+    origArr.map((row, rowI) => {
+        row.map((item, arrI) => {
+            if (item === 0) {
+                newArr[rowI].forEach((num, i) => newArr[rowI][i] = 0)
+                newArr.forEach(newRow => newRow[arrI] = 0)
+            }
+        })
+
+    })
+    console.log('newArr', newArr)
+    return newArr
+}
+
+const arr2d = [[1, 0, 1, 1, 0],
+[0, 1, 1, 1, 0],
+[1, 1, 1, 1, 1],
+[1, 0, 1, 1, 1],
+[1, 1, 1, 1, 1]]
+
+console.log('array2D(arr2d)', array2D(arr2d))
+
+
+// string rotation 
+
+function stringRotation(str1, str2) {
+    const arr1 = str1.split('')
+    const arr2 = str2.split('')
+
+    if (arr1.length !== arr2.length) {
+        return false
+    }
+
+    const letter = arr2[0]
+    const arr2Index = 0
+    let letterIndex = []
+    let arr1End = 0
+    let arr2End = 0
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] === letter) {
+            letterIndex.push(i)
+        }
+    }
+    for (let i = 0; i < letterIndex.length; i++) {
+        const arr1Letters = arr1[letterIndex[i]] + arr1[letterIndex[i] + 1]
+        const arr2Letters = letter + arr2[arr2Index + 1]
+        if (arr1Letters !== arr2Letters) {
+            continue
+        }
+        compareLetters(arr1Letters, (letterIndex[i] + 2), arr2Letters, (arr2Index + 2))
+        const remainArr1 = arr1.slice(0, letterIndex[i]).concat(arr1.slice((arr1End)))
+        const remainArr2 = arr2.slice((arr2End))
+        if (remainArr1.join('') === remainArr2.join('')) {
+            return true
+        }
+    }
+
+    function compareLetters(arr1Ltr, arr1Ind, arr2Ltr, arr2Ind) {
+        if (arr1Ind === arr1.length) {
+            arr1End = arr1Ind
+            arr2End = arr2Ind
+            return [arr1Ind, arr2Ind]
+        }
+        if (arr1Ltr !== arr2Ltr) {
+            arr1End = arr1Ind - 1
+            arr2End = arr2Ind - 1
+            return [(arr1Ind - 1), (arr2Ind - 1)]
+        }
+        const arr1Letters = arr1Ltr + arr1[arr1Ind]
+        const arr2Letters = arr2Ltr + arr2[arr2Ind]
+        compareLetters(arr1Letters, (arr1Ind + 1), arr2Letters, (arr2Ind + 1))
+    }
+
+    return false
+}
+
+const inputWords = ['amazon', 'azonam']
+// stringRotation(...inputWords)
+console.log('stringRotation(...inputWords)', stringRotation(...inputWords))
